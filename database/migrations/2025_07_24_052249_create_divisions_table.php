72f97ffeac3aeac1396e2faeb71751963f2793c3
athallahdx <athallahgamer4@gmail.com>
2025-07-30 10:26:20 +0700

    feat(division table) : add description and images fields to divisions table

--- database/migrations/2025_07_24_052249_create_divisions_table.php
+++ database/migrations/2025_07_24_052249_create_divisions_table.php
@@ -14,6 +14,8 @@ return new class extends Migration {
             $table->id();
             $table->string("name");
             $table->string("slug")->unique();
+            $table->text("description")->nullable();
+            $table->text("images")->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
