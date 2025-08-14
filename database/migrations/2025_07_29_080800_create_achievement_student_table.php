bee18def9e4e9e89b5ea74552d9c30f93d2d691b
athallahdx <athallahgamer4@gmail.com>
2025-08-14 23:05:44 +0700

    refactor(migration) : update table name in down method for achievement_student table

--- database/migrations/2025_07_29_080800_create_achievement_student_table.php
+++ database/migrations/2025_07_29_080800_create_achievement_student_table.php
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('student_achievements');
+        Schema::dropIfExists('achievement_student');
     }
 };
